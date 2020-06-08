63caf895976c25f7816ac3bc3a681433f839c181
= <=>
2020-06-08 12:21:49 +0600

    home component added under new home module

--- src/client/SimpleAppClient/src/app/app.module.ts
+++ src/client/SimpleAppClient/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/components/home.component';
+
 import { NavigationComponent } from './navigation/navigation.component';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
@@ -15,14 +15,15 @@ import { NotificationService } from './message/services/notification.service';
 import { NotificationComponent } from './message/components/notification.component';
 import { HttpErrorInterceptor } from './auth/infrastructure/error.interceptor';
 import { SharedModule } from './_shared/shared.module';
+import { HomeComponent } from './home/components/landing/home.component';
+import { HomeModule } from './home/home.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,    
-    HomeComponent,
+    NavigationComponent,        
     NotificationComponent
     
   ],
@@ -31,6 +32,7 @@ import { SharedModule } from './_shared/shared.module';
     HttpClientModule, 
     AppRoutingModule,     
     SharedModule,
+    HomeModule,
     AuthModule,
     DashboardModule, 
     ProductModule,    
---
+++ src/client/SimpleAppClient/src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/landing/home.component';
+
+@NgModule({
+    declarations: [
+        HomeComponent
+    ]
+})
+export class HomeModule {
+
+}
